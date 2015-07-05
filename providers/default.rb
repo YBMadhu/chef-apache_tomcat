efcc8a2b385705ee4ff9caefae07451ff6b171ca
Brian Clark <brian.clark@steadyserv.com>
2015-07-05 16:11:41 -0400

    make methods private

--- providers/default.rb
+++ providers/default.rb
@@ -22,85 +22,9 @@ def whyrun_supported?
   true
 end
 
-def service_name
-  new_resource.service_name || ::File.basename(new_resource.home)
-end
-
-def http_connector
-  return unless new_resource.http_port
-  http = {
-    'port' => new_resource.http_port,
-    'protocol' => 'HTTP/1.1',
-    'connectionTimeout' => '20000',
-    'URIEncoding' => 'UTF-8'
-  }
-  http['executor'] = thread_pool['name'] if thread_pool
-  http['redirectPort'] = new_resource.ssl_port if new_resource.ssl_port
-  http.merge!(new_resource.http_additional || {})
-end
-
-def ssl_connector
-  return unless new_resource.ssl_port
-  ssl = {
-    'port' => new_resource.ssl_port,
-    'protocol' => 'HTTP/1.1',
-    'connectionTimeout' => '20000',
-    'URIEncoding' => 'UTF-8',
-    'SSLEnabled' => 'true',
-    'scheme' => 'https',
-    'secure' => 'true',
-    'sslProtocol' => 'TLS',
-    'clientAuth' => 'false'
-  }
-  ssl['executor'] = thread_pool['name'] if thread_pool
-  ssl.merge!(new_resource.ssl_additional || {})
-end
-
-def thread_pool
-  return unless new_resource.pool_enabled
-  {
-    'name' => 'tomcatThreadPool',
-    'namePrefix' => 'catalina-exec-'
-  }.merge!(new_resource.pool_additional || {})
-end
-
-def ajp_connector
-  return unless new_resource.ajp_port
-  ajp = {
-    'port' => new_resource.ajp_port,
-    'protocol' => 'AJP/1.3',
-    'URIEncoding' => 'UTF-8'
-  }
-  ajp['redirectPort'] = new_resource.ssl_port if new_resource.ssl_port
-  ajp.merge!(new_resource.ajp_additional || {})
-end
-
-def access_log_valve
-  return unless new_resource.access_log_enabled
-  valve = {
-    'className' => 'org.apache.catalina.valves.AccessLogValve',
-    'prefix' => 'localhost_access_log',
-    'suffix' => '.log',
-    'rotatable' => 'false',
-    'pattern' => 'common',
-    'directory' => 'logs'
-  }
-  valve.merge!(new_resource.access_log_additional || {})
-  valve
-end
-
-def template_source(template_attrib, default)
-  return 'apache_tomcat', default unless template_attrib
-  parts = template_attrib.split(/:/, 2)
-  if parts.length == 2
-    return parts
-  else
-    return new_resource.cookbook_name.to_s, parts.first
-  end
-end
-
 action :create do
   catalina_home = new_resource.home
+  service_name = new_resource.service_name || ::File.basename(catalina_home)
   version = new_resource.version
   url = "#{node['apache_tomcat']['mirror']}/#{version}/tomcat-#{version}.tar.gz"
   tarball_name = ::File.basename(url)
@@ -112,7 +36,7 @@ action :create do
     end
     log_dir = new_resource.log_dir
   else
-    log_dir = ::File.join(new_resource.home, 'logs')
+    log_dir = ::File.join(catalina_home, 'logs')
   end
 
   group new_resource.group do
@@ -185,13 +109,13 @@ action :create do
   end
 
   link "link_logs_#{new_resource.name}" do
-    target_file ::File.join(new_resource.home, 'logs')
+    target_file ::File.join(catalina_home, 'logs')
     to new_resource.log_dir
     not_if { new_resource.log_dir.nil? }
   end
 
-  template ::File.join(new_resource.home, 'bin', 'setenv.sh') do
-    cb, src = template_source(new_resource.setenv_template, 'setenv.sh.erb')
+  cb, src = template_source(new_resource.setenv_template, 'setenv.sh.erb')
+  template ::File.join(catalina_home, 'bin', 'setenv.sh') do
     source src
     cookbook cb
     mode '0750'
@@ -203,8 +127,13 @@ action :create do
     end
   end
 
-  template ::File.join(new_resource.home, 'conf', 'server.xml') do
-    cb, src = template_source(new_resource.server_xml_template, 'server.xml.erb')
+  cb, src = template_source(new_resource.server_xml_template, 'server.xml.erb')
+  thread_pool_ = thread_pool
+  http_ = http_connector
+  ssl_ = ssl_connector
+  ajp_ = ajp_connector
+  access_log_valve_ = access_log_valve
+  template ::File.join(catalina_home, 'conf', 'server.xml') do
     source src
     cookbook cb
     mode '0640'
@@ -212,20 +141,20 @@ action :create do
     group new_resource.group
     variables(
       shutdown_port: new_resource.shutdown_port,
-      thread_pool: thread_pool,
-      http: http_connector,
-      ssl: ssl_connector,
-      ajp: ajp_connector,
+      thread_pool: thread_pool_,
+      http: http_,
+      ssl: ssl_,
+      ajp: ajp_,
       engine_valves: new_resource.engine_valves || {},
       host_valves: new_resource.host_valves || {},
-      access_log_valve: access_log_valve
+      access_log_valve: access_log_valve_
     )
     if new_resource.enable_service
       notifies :restart, "poise_service[#{service_name}]"
     end
   end
 
-  template ::File.join(new_resource.home, 'conf', 'jmxremote.access') do
+  template ::File.join(catalina_home, 'conf', 'jmxremote.access') do
     source 'jmxremote.access.erb'
     mode '0600'
     owner new_resource.user
@@ -241,7 +170,7 @@ action :create do
     end
   end
 
-  template ::File.join(new_resource.home, 'conf', 'jmxremote.password') do
+  template ::File.join(catalina_home, 'conf', 'jmxremote.password') do
     source 'jmxremote.password.erb'
     mode '0600'
     owner new_resource.user
@@ -261,9 +190,9 @@ action :create do
     end
   end
 
-  template ::File.join(new_resource.home, 'conf', 'logging.properties') do
-    cb, src = template_source(new_resource.logging_properties_template,
-                              'logging.properties.erb')
+  cb, src = template_source(new_resource.logging_properties_template,
+                            'logging.properties.erb')
+  template ::File.join(catalina_home, 'conf', 'logging.properties') do
     source src
     cookbook cb
     mode '0640'
@@ -276,14 +205,14 @@ action :create do
 
   logs = %w(catalina.out catalina.log manager.log
             host-manager.log localhost.log)
-  log_paths = logs.map { |log| ::File.join(new_resource.home, 'logs', log) }
+  log_paths = logs.map { |log| ::File.join(catalina_home, 'logs', log) }
   if access_log_valve
     fname = access_log_valve['prefix'] + access_log_valve['suffix']
-    log_paths << ::File.join(new_resource.home, 'logs', fname)
+    log_paths << ::File.join(catalina_home, 'logs', fname)
   end
 
+  cb, src = template_source(new_resource.logrotate_template, 'logrotate.erb')
   template "/etc/logrotate.d/#{service_name}" do
-    cb, src = template_source(new_resource.logrotate_template, 'logrotate.erb')
     source src
     cookbook cb
     mode '0644'
@@ -297,14 +226,89 @@ action :create do
   end
 
   poise_service service_name do # ~FC021
-    command "#{new_resource.home}/bin/catalina.sh run"
-    directory new_resource.home
+    command "#{catalina_home}/bin/catalina.sh run"
+    directory catalina_home
     provider :sysvinit if platform_family?('rhel')
     user new_resource.user
     environment(
-      CATALINA_HOME: new_resource.home,
-      CATALINA_BASE: new_resource.home)
+      CATALINA_HOME: catalina_home,
+      CATALINA_BASE: catalina_home)
     options :sysvinit, template: 'sysvinit.erb'
     action new_resource.enable_service ? :enable : :disable
+  end
+end
+
+private
+
+def http_connector
+  return unless new_resource.http_port
+  http = {
+    'port' => new_resource.http_port,
+    'protocol' => 'HTTP/1.1',
+    'connectionTimeout' => '20000',
+    'URIEncoding' => 'UTF-8'
+  }
+  http['executor'] = thread_pool['name'] if thread_pool
+  http['redirectPort'] = new_resource.ssl_port if new_resource.ssl_port
+  http.merge!(new_resource.http_additional || {})
+end
+
+def ssl_connector
+  return unless new_resource.ssl_port
+  ssl = {
+    'port' => new_resource.ssl_port,
+    'protocol' => 'HTTP/1.1',
+    'connectionTimeout' => '20000',
+    'URIEncoding' => 'UTF-8',
+    'SSLEnabled' => 'true',
+    'scheme' => 'https',
+    'secure' => 'true',
+    'sslProtocol' => 'TLS',
+    'clientAuth' => 'false'
+  }
+  ssl['executor'] = thread_pool['name'] if thread_pool
+  ssl.merge!(new_resource.ssl_additional || {})
+end
+
+def thread_pool
+  return unless new_resource.pool_enabled
+  {
+    'name' => 'tomcatThreadPool',
+    'namePrefix' => 'catalina-exec-'
+  }.merge!(new_resource.pool_additional || {})
+end
+
+def ajp_connector
+  return unless new_resource.ajp_port
+  ajp = {
+    'port' => new_resource.ajp_port,
+    'protocol' => 'AJP/1.3',
+    'URIEncoding' => 'UTF-8'
+  }
+  ajp['redirectPort'] = new_resource.ssl_port if new_resource.ssl_port
+  ajp.merge!(new_resource.ajp_additional || {})
+end
+
+def access_log_valve
+  return unless new_resource.access_log_enabled
+  valve = {
+    'className' => 'org.apache.catalina.valves.AccessLogValve',
+    'prefix' => 'localhost_access_log',
+    'suffix' => '.log',
+    'rotatable' => 'false',
+    'pattern' => 'common',
+    'directory' => 'logs'
+  }
+  valve.merge!(new_resource.access_log_additional || {})
+  valve
+end
+
+def template_source(template_attrib, default)
+  return 'apache_tomcat', default unless template_attrib
+  parts = template_attrib.split(/:/, 2)
+  if parts.length == 2
+    return parts
+  else
+    return new_resource.cookbook_name.to_s, parts.first
   end
 end
