a02cac998c03d921e28ed6958ce29103ef1c651c
Brian Clark <brian@clark.zone>
2015-07-11 08:37:10 -0400

    rearrange default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,17 @@
 # rubocop:disable Metrics/LineLength
 
-default['apache_tomcat']['user'] = 'tomcat'
-default['apache_tomcat']['group'] = 'tomcat'
+# Default home and base settings
+default['apache_tomcat']['install_path'] = '/usr/local/tomcat'
+default['apache_tomcat']['instance_path'] = '/var/lib/tomcat'
 
+# Default installation settings
 default['apache_tomcat']['mirror']  = 'https://repository.apache.org/content/repositories/releases/org/apache/tomcat/tomcat'
 default['apache_tomcat']['version'] = '7.0.56'
 default['apache_tomcat']['checksum'] = 'c0ca44be20bccebbb043ccd7ab5ea4d94060fdde6bb84812f3da363955dae5bb'
-default['apache_tomcat']['install_path'] = '/usr/local/tomcat'
-default['apache_tomcat']['instance_path'] = '/var/lib/tomcat'
+
+# General instance settings
+default['apache_tomcat']['user'] = 'tomcat'
+default['apache_tomcat']['group'] = 'tomcat'
 default['apache_tomcat']['service_name'] = nil
 default['apache_tomcat']['enable_service'] = true
 default['apache_tomcat']['webapps_mode'] = nil
