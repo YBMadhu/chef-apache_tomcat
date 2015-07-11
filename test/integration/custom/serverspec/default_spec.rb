600541d836044303a6365d8230c29eb68e61cd98
Brian Clark <brian@clark.zone>
2015-07-11 02:48:56 -0400

    improve integration tests

--- test/integration/custom/serverspec/default_spec.rb
+++ test/integration/custom/serverspec/default_spec.rb
@@ -3,21 +3,96 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe file('/opt/tomcat7/bin/setenv.sh') do
+describe user('tomcat') do
+  it { should exist }
+  it { should belong_to_group 'tomcat' }
+end
+
+describe file('/opt/tomcat-7.0.56') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/opt/tomcat7') do
+  it { should be_directory }
+  it { should be_symlink }
+  it { should be_linked_to '/opt/tomcat-7.0.56' }
+end
+
+%w(bin conf lib).each do |dir|
+  describe file("/var/tomcat/#{dir}") do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'tomcat' }
+    it { should be_mode 755 }
+  end
+end
+
+%w(temp work).each do |dir|
+  describe file("/var/tomcat/#{dir}") do
+    it { should be_directory }
+    it { should be_owned_by 'tomcat' }
+    it { should be_grouped_into 'tomcat' }
+    it { should be_mode 755 }
+  end
+end
+
+describe file('/var/tomcat/webapps') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should be_mode 755 }
+end
+
+describe file('/var/tomcat/webapps/manager') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should be_mode 755 }
+end
+
+%w(catalina.policy catalina.properties web.xml context.xml).each do |conf_file|
+  describe file("/var/tomcat/conf/#{conf_file}") do
+    it { should be_file }
+    it { should be_linked_to "/opt/tomcat7/conf/#{conf_file}"}
+  end
+end
+
+describe file('/var/tomcat/bin/setenv.sh') do
   it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
   its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Xms256m"' }
   its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Xmx512m"' }
 end
 
-describe file('/opt/tomcat7/conf/server.xml') do
+describe file('/var/tomcat/conf/server.xml') do
   it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
 end
 
-describe file('/opt/tomcat7/conf/logging.properties') do
+describe file('/var/tomcat/conf/logging.properties') do
   it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
 end
 
-describe service('tomcat7') do
+describe file('/var/tomcat/conf/tomcat-users.xml') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
+  its(:content) { should include '<role rolename="manager-gui" />' }
+  its(:content) { should include '<user username="joedirt" password="mullet" roles="manager-gui" />' }
+end
+
+describe service('tomcat-default') do
+  it { should be_enabled }
   it { should be_running }
 end
 
@@ -37,13 +112,13 @@ describe port(9443) do
   it { should be_listening }
 end
 
-describe file('/etc/logrotate.d/tomcat7') do
+describe file('/etc/logrotate.d/tomcat') do
   it { should be_file }
-  its(:content) { should include '/opt/tomcat7/logs/catalina.out' }
-  its(:content) { should include '/opt/tomcat7/logs/catalina.log' }
-  its(:content) { should include '/opt/tomcat7/logs/manager.log' }
-  its(:content) { should include '/opt/tomcat7/logs/localhost.log' }
-  its(:content) { should include '/opt/tomcat7/logs/host-manager.log' }
+  its(:content) { should include '/var/tomcat/logs/catalina.out' }
+  its(:content) { should include '/var/tomcat/logs/catalina.log' }
+  its(:content) { should include '/var/tomcat/logs/manager.log' }
+  its(:content) { should include '/var/tomcat/logs/localhost.log' }
+  its(:content) { should include '/var/tomcat/logs/host-manager.log' }
   its(:content) { should include 'rotate 12' }
   its(:content) { should include 'monthly' }
 end
@@ -76,21 +151,21 @@ describe port(9599) do
   it { should be_listening }
 end
 
-describe file('/opt/tomcat7/bin/setenv.sh') do
+describe file('/var/tomcat/bin/setenv.sh') do
   its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote"' }
   its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote.ssl=false"' }
   its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote.authenticate=true"' }
-  its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote.access.file=/opt/tomcat7/conf/jmxremote.access"' }
-  its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote.password.file=/opt/tomcat7/conf/jmxremote.password"' }
+  its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote.access.file=/var/tomcat/conf/jmxremote.access"' }
+  its(:content) { should include 'CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.jmxremote.password.file=/var/tomcat/conf/jmxremote.password"' }
 end
 
-describe file('/opt/tomcat7/conf/jmxremote.access') do
+describe file('/var/tomcat/conf/jmxremote.access') do
   it { should be_file }
   its(:content) { should include 'monitorRole readonly' }
   its(:content) { should include 'controlRole readwrite' }
 end
 
-describe file('/opt/tomcat7/conf/jmxremote.password') do
+describe file('/var/tomcat/conf/jmxremote.password') do
   it { should be_file }
   its(:content) { should include 'monitorRole mymonitorpw' }
   its(:content) { should include 'controlRole mycontrolpw' }
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,22 +2,90 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe file('/opt/tomcat/bin/setenv.sh') do
-  it { should be_file }
+describe user('tomcat') do
+  it { should exist }
+  it { should belong_to_group 'tomcat' }
 end
 
-describe file('/opt/tomcat/conf/server.xml') do
-  it { should be_file }
-  its(:content) { should_not include '-Xms' }
-  its(:content) { should_not include '-Xmx' }
-  its(:content) { should_not include 'jmxremote' }
+describe file('/usr/local/tomcat-7.0.56') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
 end
 
-describe file('/opt/tomcat/conf/logging.properties') do
+describe file('/usr/local/tomcat') do
+  it { should be_directory }
+  it { should be_symlink }
+  it { should be_linked_to '/usr/local/tomcat-7.0.56' }
+end
+
+%w(bin conf lib).each do |dir|
+  describe file("/var/lib/tomcat/#{dir}") do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'tomcat' }
+    it { should be_mode 755 }
+  end
+end
+
+%w(temp work).each do |dir|
+  describe file("/var/lib/tomcat/#{dir}") do
+    it { should be_directory }
+    it { should be_owned_by 'tomcat' }
+    it { should be_grouped_into 'tomcat' }
+    it { should be_mode 755 }
+  end
+end
+
+describe file('/var/lib/tomcat/webapps') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should be_mode 775 }
+end
+
+describe file('/var/lib/tomcat/webapps/manager') do
+  it { should_not be_directory }
+end
+
+%w(catalina.policy catalina.properties web.xml context.xml).each do |conf_file|
+  describe file("/var/lib/tomcat/conf/#{conf_file}") do
+    it { should be_file }
+    it { should be_linked_to "/usr/local/tomcat/conf/#{conf_file}"}
+  end
+end
+
+describe file('/var/lib/tomcat/bin/setenv.sh') do
   it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
+end
+
+describe file('/var/lib/tomcat/conf/server.xml') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
+end
+
+describe file('/var/lib/tomcat/conf/logging.properties') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
+end
+
+describe file('/var/lib/tomcat/conf/tomcat-users.xml') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'tomcat' }
+  it { should  be_mode 640 }
+  its(:content) { should include "<tomcat-users>\n</tomcat-users>" }
 end
 
 describe service('tomcat') do
+  it { should be_enabled }
   it { should be_running }
 end
 
@@ -35,11 +103,11 @@ end
 
 describe file('/etc/logrotate.d/tomcat') do
   it { should be_file }
-  its(:content) { should include '/opt/tomcat/logs/catalina.out' }
-  its(:content) { should include '/opt/tomcat/logs/catalina.log' }
-  its(:content) { should include '/opt/tomcat/logs/manager.log' }
-  its(:content) { should include '/opt/tomcat/logs/localhost.log' }
-  its(:content) { should include '/opt/tomcat/logs/host-manager.log' }
+  its(:content) { should include '/var/lib/tomcat/logs/catalina.out' }
+  its(:content) { should include '/var/lib/tomcat/logs/catalina.log' }
+  its(:content) { should include '/var/lib/tomcat/logs/manager.log' }
+  its(:content) { should include '/var/lib/tomcat/logs/localhost.log' }
+  its(:content) { should include '/var/lib/tomcat/logs/host-manager.log' }
   its(:content) { should include 'rotate 4' }
   its(:content) { should include 'weekly' }
 end
@@ -48,34 +116,30 @@ end
 #   it { should be_file }
 # end
 
-describe file('/opt/tomcat/logs/catalina.log') do
+describe file('/var/lib/tomcat/logs/catalina.log') do
   it { should be_file }
 end
 
-describe file('/opt/tomcat/logs/manager.log') do
+describe file('/var/lib/tomcat/logs/manager.log') do
   it { should be_file }
 end
 
-describe file('/opt/tomcat/logs/host-manager.log') do
+describe file('/var/lib/tomcat/logs/localhost.log') do
   it { should be_file }
 end
 
-describe file('/opt/tomcat/logs/localhost.log') do
-  it { should be_file }
-end
-
-describe file('/opt/tomcat/logs/localhost_access_log.log') do
+describe file('/var/lib/tomcat/logs/localhost_access_log.log') do
   it { should_not be_file }
 end
 
-describe file('/opt/tomcat/conf/server.xml') do
+describe file('/var/lib/tomcat/conf/server.xml') do
   its(:content) { should_not include 'jmxremote' }
 end
 
-describe file('/opt/tomcat7/conf/jmxremote.access') do
+describe file('/var/lib/tomcat7/conf/jmxremote.access') do
   it { should_not be_file }
 end
 
-describe file('/opt/tomcat7/conf/jmxremote.password') do
+describe file('/var/lib/tomcat7/conf/jmxremote.password') do
   it { should_not be_file }
 end
